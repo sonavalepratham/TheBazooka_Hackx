4fcd58433bdd4ea6afa77325e1d35dfa4dbbd4f3
Satyam Baldawa <baldawasatyam30@gmail.com>
2021-10-10 05:48:04 +0530

    all set commit

--- HackxHealth/settings.py
+++ HackxHealth/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -124,6 +124,7 @@ EMAIL_HOST_PASSWORD = 'hackxbazooka'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
